1090b7d4aa7b14c03066eee476d907ea6a340072
rgruenewald <4582139+rgruenewald@users.noreply.github.com>
2026-04-05 18:49:43 +0200

    map anymak symbol layer including shift-row variants

--- config/keys_de_german.h
+++ config/keys_de_german.h
@@ -19,3 +19,24 @@
 #undef DE_DEG
 #define DE_DEGREE (LS(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_GRAVE_ACCENT_AND_TILDE)))
 #define DE_DEG (DE_DEGREE)
+
+/* µ on DE-DE: AltGr+M */
+#ifndef DE_MICRO
+#define DE_MICRO (RA(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_M)))
+#endif
+#ifndef DE_MU
+#define DE_MU (DE_MICRO)
+#endif
+
+/* ¥ on DE-DE: AltGr+Y (Y key position in DE layout) */
+#ifndef DE_YEN
+#define DE_YEN (RA(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_Z)))
+#endif
+
+/* ß on DE-DE (US-Intl): AltGr+S */
+#ifndef DE_SHARP_S
+#define DE_SHARP_S (RA(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_S)))
+#endif
+#ifndef DE_SS
+#define DE_SS (DE_SHARP_S)
+#endif
